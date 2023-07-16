ac7bfb92e1df4d94f01e7c18bb28c5701b3010af
Koji5 <76521976+Koji5@users.noreply.github.com>
2023-07-16 12:38:20 +0900

    userモデルの単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: "は全角（漢字・ひらがな・カタカナ）で入力してください" }
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: "は全角（漢字・ひらがな・カタカナ）で入力してください" }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力してください" }
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力してください" }
-  validates :birth_date, presence: true
+#  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: "は全角（漢字・ひらがな・カタカナ）で入力してください" }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :birth_date, presence: true, format: { with: /\A\d{4}-\d{2}-\d{2}\z/ }
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,18 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.name}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    first_name            {'山田'}
+    last_name             {'太郎'}
+    first_name_kana       {'ヤマダ'}
+    last_name_kana        {'タロウ'}
+# Rubyのバージョンが2.7以上で、Fakerのバージョンが2.22.0以上の場合、以下が使用できる。
+#   first_name            {Faker::Name.ja_first_name}
+#   last_name             {Faker::Name.ja_last_name}
+#   first_name_kana       {Faker::Japanese::Katakana.first_name}
+#   last_name_kana        {Faker::Japanese::Katakana.last_name}
+    birth_date            {Faker::Date.birthday}
+  end
+end
