b9749c6debe115ea66df1de4c1a7803156b63997
Koji5 <76521976+Koji5@users.noreply.github.com>
2023-07-20 19:56:56 +0900

    OrderHistoryモデル・OrderAddressモデルにAssociationを追加

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,2 +1,8 @@
 class OrderAddress < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+  belongs_to :order_history
+
+  #選択が「---」の時は保存できないようにする
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 end
