449b9b05c7f758f81452b605b1790d7f95c57544
Koji5 <76521976+Koji5@users.noreply.github.com>
2023-07-20 20:44:48 +0900

    Itemモデルにバリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,10 +8,16 @@ class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :user
 
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :info, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, only_integer: true }
+
   #選択が「---」の時は保存できないようにする
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :sales_status_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :shipping_fee_status_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :scheduled_delivery_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+
 end
