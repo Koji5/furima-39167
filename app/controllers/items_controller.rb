098e8b06470da6a8ff09f97c38ddbacb5dbe87e8
Koji5 <76521976+Koji5@users.noreply.github.com>
2023-07-23 17:32:44 +0900

    編集可否の条件分岐

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   before_action :set_item, only: [:show, :edit, :update]
+  before_action :move_to_index, only: [:edit, :update]
 
   def index
     @items = Item.order(created_at: :desc).limit(9)
@@ -51,5 +52,12 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    if current_user.id != @item.user_id
+      redirect_to root_path
+    end
+    # TODO:売却済み商品かどうかの条件分岐
   end
 end
