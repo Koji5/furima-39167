cf4a9d3b8f2d8642addf34bc80eff86db13f2866
Koji5 <76521976+Koji5@users.noreply.github.com>
2023-07-30 16:14:43 +0900

    Formオブジェクトの作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,27 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: :index
-  before_action :set_item, only: :index
+  before_action :set_item, only:[:index, :create]
   before_action :move_to_index, only: :index
 
   def index
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.valid?
+      @order.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
 
   private
+
+  def order_params
+    params.require(:order).permit(:postal_code, :prefecture_id, :city, :address, :building, :price).merge(item_id: @item.id, user_id: current_user.id)
+  end
 
   def set_item
     @item = Item.find(params[:item_id])
