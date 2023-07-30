fd9ef17942d3515e678a4d3c61f3a54145b5a87d
Koji5 <76521976+Koji5@users.noreply.github.com>
2023-07-30 14:12:07 +0900

    購入画面に商品を表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,21 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: :index
+  before_action :set_item, only: :index
+  before_action :move_to_index, only: :index
+
   def index
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def move_to_index
+    if current_user.id == @item.user_id
+      redirect_to root_path
+    end
+    # TODO:売却済み商品かどうかの条件分岐
   end
 end
